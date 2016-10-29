e3dcf90f9b084d89dbde29de04f1671d6fd3bfde
AsteriX <asterix3x@gmail.com>
2016-10-29 14:08:32 +0300

    Test ReCreated

---
+++ test/ua/com/asterix/xo/model/PlayerTest.java
@@ -0,0 +1,31 @@
+package ua.com.asterix.xo.model;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class PlayerTest {
+    @Test
+    public void getName() throws Exception {
+        final String inputValue = "Денис";
+        final String expectedValue = inputValue;
+
+        final Player player = new Player(inputValue, null);
+
+        final String actualValue = player.getName();
+
+        TestCase.assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void getFigure() throws Exception {
+        final Figure inputValue = Figure.X;
+        final Figure expectedValue = inputValue;
+
+        final Player player = new Player(null, inputValue);
+
+        final Figure actualValue = Figure.X;
+
+        TestCase.assertEquals(expectedValue, actualValue);
+    }
+
+}
