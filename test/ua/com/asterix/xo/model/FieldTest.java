a28f436df2ad673f14588d39e7f0d99a887b04ae
AsteriX <asterix3x@gmail.com>
2016-10-29 19:54:06 +0300

    FieldTest created

---
+++ test/ua/com/asterix/xo/model/FieldTest.java
@@ -0,0 +1,22 @@
+package ua.com.asterix.xo.model;
+
+import org.junit.Test;
+
+public class FieldTest {
+    @Test
+    public void getFieldSize() throws Exception {
+        Field field = new Field();
+
+    }
+
+    @Test
+    public void setFigure() throws Exception {
+
+    }
+
+    @Test
+    public void getFigure() throws Exception {
+
+    }
+
+}
