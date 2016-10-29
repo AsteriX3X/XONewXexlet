d187f1c1e7c63ef17080277d99f76433bf1de21a
AsteriX <asterix3x@gmail.com>
2016-10-29 13:46:05 +0300

    Test created

---
+++ tst/ua/com/asterix/xo/model/PlayerTest.java
@@ -0,0 +1,16 @@
+package ua.com.asterix.xo.model;
+
+import org.junit.Test;
+
+public class PlayerTest {
+    @Test
+    public void getName() throws Exception {
+
+    }
+
+    @Test
+    public void getFigure() throws Exception {
+
+    }
+
+}
